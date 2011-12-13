mausys/gclient

commit 048966aac9cb1e5e57645f2a8ac7c0b76530a9bc

Enforces using cwd in all svn calls.
That helps weed out some issues faces with svn plus helped me figure out some
misuses.

Most of the commands have been implicitly depending on os.getcwd(). This change
makes it always consistent and clear when dependence on the current directory is
needed.

Remove default arguments to scm.SVN.GenerateDiff and a few other calls to be
sure the refactoring was done right.

R=dpranke@chromium.org
BUG=
TEST=make sure most commands aren't broke


Review URL: http://codereview.chromium.org/8771042

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@114262 0039d316-1c4b-4281-b951-d872f2087c98

scm.py
@@ -398,13 +398,13 @@ class SVN(object):
   current_version = None
 
   @staticmethod
-  def Capture(args, **kwargs):
+  def Capture(args, cwd, **kwargs):
     """Always redirect stderr.
 
     Throws an exception if non-0 is returned.
     """
     return subprocess2.check_output(
-        ['svn'] + args, stderr=subprocess2.PIPE, **kwargs)
+        ['svn'] + args, stderr=subprocess2.PIPE, cwd=cwd, **kwargs)
 
   @staticmethod
   def RunAndGetFileList(verbose, args, cwd, file_list, stdout=None):
@@ -514,13 +514,30 @@ class SVN(object):
       break
 
   @staticmethod
-  def CaptureInfo(cwd):
+  def CaptureRemoteInfo(url):
+    """Returns a dictionary from the svn info output for the given url.
+
+    Throws an exception if svn info fails.
+    """
+    assert isinstance(url, str)
+    return SVN._CaptureInfo([url], None)
+
+  @staticmethod
+  def CaptureLocalInfo(files, cwd):
+    """Returns a dictionary from the svn info output for the given files.
+
+    Throws an exception if svn info fails.
+    """
+    assert isinstance(files, (list, tuple))
+    return SVN._CaptureInfo(files, cwd)
+
+  @staticmethod
+  def _CaptureInfo(files, cwd):
     """Returns a dictionary from the svn info output for the given file.
 
     Throws an exception if svn info fails."""
     result = {}
-    output = SVN.Capture(['info', '--xml', cwd])
-    info = ElementTree.XML(output)
+    info = ElementTree.XML(SVN.Capture(['info', '--xml'] + files, cwd))
     if info is None:
       return result
     entry = info.find('entry')
@@ -563,10 +580,10 @@ class SVN(object):
     Returns:
       Int base revision
     """
-    return SVN.CaptureInfo(cwd).get('Revision')
+    return SVN.CaptureLocalInfo([], cwd).get('Revision')
 
   @staticmethod
-  def CaptureStatus(files):
+  def CaptureStatus(files, cwd):
     """Returns the svn 1.5 svn status emulated output.
 
     @files can be a string (one file) or a list of files.
@@ -598,7 +615,7 @@ class SVN(object):
       'replaced': 'R',
       'unversioned': '?',
     }
-    dom = ElementTree.XML(SVN.Capture(command))
+    dom = ElementTree.XML(SVN.Capture(command, cwd))
     results = []
     if dom is None:
       return results
@@ -645,9 +662,10 @@ class SVN(object):
     return results
 
   @staticmethod
-  def IsMoved(filename):
+  def IsMoved(filename, cwd):
     """Determine if a file has been added through svn mv"""
-    return SVN.IsMovedInfo(SVN.CaptureInfo(filename))
+    assert isinstance(filename, basestring)
+    return SVN.IsMovedInfo(SVN.CaptureLocalInfo([filename], cwd))
 
   @staticmethod
   def IsMovedInfo(info):
@@ -657,7 +675,7 @@ class SVN(object):
             info.get('Schedule') == 'add')
 
   @staticmethod
-  def GetFileProperty(filename, property_name):
+  def GetFileProperty(filename, property_name, cwd):
     """Returns the value of an SVN property for the given file.
 
     Args:
@@ -670,12 +688,12 @@ class SVN(object):
       empty string is also returned.
     """
     try:
-      return SVN.Capture(['propget', property_name, filename])
+      return SVN.Capture(['propget', property_name, filename], cwd)
     except subprocess2.CalledProcessError:
       return ''
 
   @staticmethod
-  def DiffItem(filename, full_move=False, revision=None):
+  def DiffItem(filename, cwd, full_move, revision):
     """Diffs a single file.
 
     Should be simple, eh? No it isn't.
@@ -693,15 +711,18 @@ class SVN(object):
     try:
       # Use "svn info" output instead of os.path.isdir because the latter fails
       # when the file is deleted.
-      return SVN._DiffItemInternal(filename, SVN.CaptureInfo(filename),
-                                   bogus_dir,
-                                   full_move=full_move, revision=revision)
+      return SVN._DiffItemInternal(
+          filename,
+          cwd,
+          SVN.CaptureLocalInfo([filename], cwd),
+          bogus_dir,
+          full_move,
+          revision)
     finally:
       gclient_utils.RemoveDirectory(bogus_dir)
 
   @staticmethod
-  def _DiffItemInternal(filename, info, bogus_dir, full_move=False,
-                        revision=None):
+  def _DiffItemInternal(filename, cwd, info, bogus_dir, full_move, revision):
     """Grabs the diff data."""
     command = ["diff", "--config-dir", bogus_dir, filename]
     if revision:
@@ -737,7 +758,7 @@ class SVN(object):
       else:
         if info.get("Node Kind") != "directory":
           # svn diff on a mv/cp'd file outputs nothing if there was no change.
-          data = SVN.Capture(command)
+          data = SVN.Capture(command, cwd)
           if not data:
             # We put in an empty Index entry so upload.py knows about them.
             data = "Index: %s\n" % filename.replace(os.sep, '/')
@@ -746,7 +767,7 @@ class SVN(object):
       if info.get("Node Kind") != "directory":
         # Normal simple case.
         try:
-          data = SVN.Capture(command)
+          data = SVN.Capture(command, cwd)
         except subprocess2.CalledProcessError:
           if revision:
             data = GenFakeDiff(filename)
@@ -756,7 +777,7 @@ class SVN(object):
     return data
 
   @staticmethod
-  def GenerateDiff(filenames, root=None, full_move=False, revision=None):
+  def GenerateDiff(filenames, cwd, full_move, revision):
     """Returns a string containing the diff for the given file list.
 
     The files in the list should either be absolute paths or relative to the
@@ -765,9 +786,7 @@ class SVN(object):
     The diff will always use relative paths.
     """
     assert isinstance(filenames, (list, tuple))
-    previous_cwd = os.getcwd()
-    root = root or SVN.GetCheckoutRoot(previous_cwd)
-    root = os.path.normcase(os.path.join(root, ''))
+    root = os.path.normcase(os.path.join(cwd, ''))
     def RelativePath(path, root):
       """We must use relative paths."""
       if os.path.normcase(path).startswith(root):
@@ -781,11 +800,10 @@ class SVN(object):
     # config directory, which gets around these problems.
     bogus_dir = tempfile.mkdtemp()
     try:
-      os.chdir(root)
       # Cleanup filenames
       filenames = [RelativePath(f, root) for f in filenames]
       # Get information about the modified items (files and directories)
-      data = dict([(f, SVN.CaptureInfo(f)) for f in filenames])
+      data = dict([(f, SVN.CaptureLocalInfo([f], root)) for f in filenames])
       diffs = []
       if full_move:
         # Eliminate modified files inside moved/copied directory.
@@ -805,12 +823,12 @@ class SVN(object):
             # for now, the most common case is a head copy,
             # so let's just encode that as a straight up cp.
             srcurl = info.get('Copied From URL')
-            root = info.get('Repository Root')
+            file_root = info.get('Repository Root')
             rev = int(info.get('Copied From Rev'))
-            assert srcurl.startswith(root)
-            src = srcurl[len(root)+1:]
+            assert srcurl.startswith(file_root)
+            src = srcurl[len(file_root)+1:]
             try:
-              srcinfo = SVN.CaptureInfo(srcurl)
+              srcinfo = SVN.CaptureRemoteInfo(srcurl)
             except subprocess2.CalledProcessError, e:
               if not 'Not a valid URL' in e.stderr:
                 raise
@@ -818,7 +836,7 @@ class SVN(object):
               # revision the file was deleted.
               srcinfo = {'Revision': rev}
             if (srcinfo.get('Revision') != rev and
-                SVN.Capture(['diff', '-r', '%d:head' % rev, srcurl])):
+                SVN.Capture(['diff', '-r', '%d:head' % rev, srcurl], cwd)):
               metaheaders.append("#$ svn cp -r %d %s %s "
                                  "### WARNING: note non-trunk copy\n" %
                                  (rev, src, filename))
@@ -832,9 +850,8 @@ class SVN(object):
           diffs.append("### END SVN COPY METADATA\n")
       # Now ready to do the actual diff.
       for filename in sorted(data.iterkeys()):
-        diffs.append(SVN._DiffItemInternal(filename, data[filename], bogus_dir,
-                                           full_move=full_move,
-                                           revision=revision))
+        diffs.append(SVN._DiffItemInternal(
+            filename, cwd, data[filename], bogus_dir, full_move, revision))
       # Use StringIO since it can be messy when diffing a directory move with
       # full_move=True.
       buf = cStringIO.StringIO()
@@ -844,14 +861,13 @@ class SVN(object):
       buf.close()
       return result
     finally:
-      os.chdir(previous_cwd)
       gclient_utils.RemoveDirectory(bogus_dir)
 
   @staticmethod
-  def GetEmail(repo_root):
+  def GetEmail(cwd):
     """Retrieves the svn account which we assume is an email address."""
     try:
-      infos = SVN.CaptureInfo(repo_root)
+      infos = SVN.CaptureLocalInfo([], cwd)
     except subprocess2.CalledProcessError:
       return None
 
@@ -904,36 +920,36 @@ class SVN(object):
     return values
 
   @staticmethod
-  def GetCheckoutRoot(directory):
+  def GetCheckoutRoot(cwd):
     """Returns the top level directory of the current repository.
 
     The directory is returned as an absolute path.
     """
-    directory = os.path.abspath(directory)
+    cwd = os.path.abspath(cwd)
     try:
-      info = SVN.CaptureInfo(directory)
+      info = SVN.CaptureLocalInfo([], cwd)
       cur_dir_repo_root = info['Repository Root']
       url = info['URL']
     except subprocess2.CalledProcessError:
       return None
     while True:
-      parent = os.path.dirname(directory)
+      parent = os.path.dirname(cwd)
       try:
-        info = SVN.CaptureInfo(parent)
+        info = SVN.CaptureLocalInfo([], parent)
         if (info['Repository Root'] != cur_dir_repo_root or
             info['URL'] != os.path.dirname(url)):
           break
         url = info['URL']
       except subprocess2.CalledProcessError:
         break
-      directory = parent
-    return GetCasedPath(directory)
+      cwd = parent
+    return GetCasedPath(cwd)
 
   @classmethod
   def AssertVersion(cls, min_version):
     """Asserts svn's version is at least min_version."""
     if cls.current_version is None:
-      cls.current_version = cls.Capture(['--version']).split()[2]
+      cls.current_version = cls.Capture(['--version'], None).split()[2]
     current_version_list = map(only_int, cls.current_version.split('.'))
     for min_ver in map(int, min_version.split('.')):
       ver = current_version_list.pop(0)
@@ -944,16 +960,16 @@ class SVN(object):
     return (True, cls.current_version)
 
   @staticmethod
-  def Revert(repo_root, callback=None, ignore_externals=False):
-    """Reverts all svn modifications in repo_root, including properties.
+  def Revert(cwd, callback=None, ignore_externals=False):
+    """Reverts all svn modifications in cwd, including properties.
 
     Deletes any modified files or directory.
 
     A "svn update --revision BASE" call is required after to revive deleted
     files.
     """
-    for file_status in SVN.CaptureStatus(repo_root):
-      file_path = os.path.join(repo_root, file_status[1])
+    for file_status in SVN.CaptureStatus(None, cwd):
+      file_path = os.path.join(cwd, file_status[1])
       if (ignore_externals and
           file_status[0][0] == 'X' and
           file_status[0][1:].isspace()):
@@ -985,11 +1001,11 @@ class SVN(object):
           not file_status[0][1:].isspace()):
         # Added, deleted file requires manual intervention and require calling
         # revert, like for properties.
-        if not os.path.isdir(repo_root):
+        if not os.path.isdir(cwd):
           # '.' was deleted. It's not worth continuing.
           return
         try:
-          SVN.Capture(['revert', file_status[1]], cwd=repo_root)
+          SVN.Capture(['revert', file_status[1]], cwd=cwd)
         except subprocess2.CalledProcessError:
           if not os.path.exists(file_path):
             continue

gcl.py
@@ -87,7 +87,10 @@ def CheckHomeForFile(filename):
 
 def UnknownFiles():
   """Runs svn status and returns unknown files."""
-  return [item[1] for item in SVN.CaptureStatus([]) if item[0][0] == '?']
+  return [
+      item[1] for item in SVN.CaptureStatus([], GetRepositoryRoot())
+      if item[0][0] == '?'
+  ]
 
 
 def GetRepositoryRoot():
@@ -138,7 +141,7 @@ def GetCachedFile(filename, max_age=60*60*24*3, use_root=False):
       return None
     if (not os.path.exists(cached_file) or
         (time.time() - os.stat(cached_file).st_mtime) > max_age):
-      dir_info = SVN.CaptureInfo('.')
+      dir_info = SVN.CaptureLocalInfo([], '.')
       repo_root = dir_info['Repository Root']
       if use_root:
         url_path = repo_root
@@ -548,8 +551,7 @@ class ChangeInfo(object):
     files = values['files']
     if update_status:
       for item in files[:]:
-        filename = os.path.join(local_root, item[1])
-        status_result = SVN.CaptureStatus(filename)
+        status_result = SVN.CaptureStatus(item[1], local_root)
         if not status_result or not status_result[0][0]:
           # File has been reverted.
           save = True
@@ -678,7 +680,7 @@ def GetModifiedFiles():
       files_in_cl[filename] = change_info.name
 
   # Get all the modified files.
-  status_result = SVN.CaptureStatus(None)
+  status_result = SVN.CaptureStatus(None, GetRepositoryRoot())
   for line in status_result:
     status = line[0]
     filename = line[1]
@@ -733,8 +735,9 @@ def ListFiles(show_unknown_files):
   return 0
 
 
-def GenerateDiff(files, root=None):
-  return SVN.GenerateDiff(files, root=root)
+def GenerateDiff(files):
+  return SVN.GenerateDiff(
+      files, GetRepositoryRoot(), full_move=False, revision=None)
 
 
 def OptionallyDoPresubmitChecks(change_info, committing, args):
@@ -1037,7 +1040,14 @@ def CMDcommit(change_info, args):
 def CMDchange(args):
   """Creates or edits a changelist.
 
-  Only scans the current directory and subdirectories."""
+  Only scans the current directory and subdirectories.
+  """
+  # Verify the user is running the change command from a read-write checkout.
+  svn_info = SVN.CaptureLocalInfo([], '.')
+  if not svn_info:
+    ErrorExit("Current checkout is unversioned.  Please retry with a versioned "
+              "directory.")
+
   if len(args) == 0:
     # Generate a random changelist name.
     changename = GenerateChangeName()
@@ -1046,12 +1056,6 @@ def CMDchange(args):
   else:
     changename = args[0]
   change_info = ChangeInfo.Load(changename, GetRepositoryRoot(), False, True)
-
-  # Verify the user is running the change command from a read-write checkout.
-  svn_info = SVN.CaptureInfo('.')
-  if not svn_info:
-    ErrorExit("Current checkout is unversioned.  Please retry with a versioned "
-              "directory.")
 
   if len(args) == 2:
     if not os.path.isfile(args[1]):
@@ -1439,6 +1443,7 @@ def main(argv):
   except upload.ClientLoginError, e:
     print >> sys.stderr, 'Got an exception logging in to Rietveld'
     print >> sys.stderr, str(e)
+    return 1
   except urllib2.HTTPError, e:
     if e.code != 500:
       raise

gclient_scm.py
@@ -711,8 +711,9 @@ class SVNWrapper(SCMWrapper):
   def GetRevisionDate(self, revision):
     """Returns the given revision's date in ISO-8601 format (which contains the
     time zone)."""
-    date = scm.SVN.Capture(['propget', '--revprop', 'svn:date', '-r', revision,
-                            os.path.join(self.checkout_path, '.')])
+    date = scm.SVN.Capture(
+        ['propget', '--revprop', 'svn:date', '-r', revision],
+        os.path.join(self.checkout_path, '.'))
     return date.strip()
 
   def cleanup(self, options, args, file_list):
@@ -796,7 +797,8 @@ class SVNWrapper(SCMWrapper):
 
     # Get the existing scm url and the revision number of the current checkout.
     try:
-      from_info = scm.SVN.CaptureInfo(os.path.join(self.checkout_path, '.'))
+      from_info = scm.SVN.CaptureLocalInfo(
+          [], os.path.join(self.checkout_path, '.'))
     except (gclient_utils.Error, subprocess2.CalledProcessError):
       raise gclient_utils.Error(
           ('Can\'t update/checkout %s if an unversioned directory is present. '
@@ -809,14 +811,16 @@ class SVNWrapper(SCMWrapper):
             self.checkout_path, from_info))
 
     # Look for locked directories.
-    dir_info = scm.SVN.CaptureStatus(os.path.join(self.checkout_path, '.'))
+    dir_info = scm.SVN.CaptureStatus(
+        None, os.path.join(self.checkout_path, '.'))
     if any(d[0][2] == 'L' for d in dir_info):
       try:
         self._Run(['cleanup', self.checkout_path], options)
       except subprocess2.CalledProcessError, e:
         # Get the status again, svn cleanup may have cleaned up at least
         # something.
-        dir_info = scm.SVN.CaptureStatus(os.path.join(self.checkout_path, '.'))
+        dir_info = scm.SVN.CaptureStatus(
+            None, os.path.join(self.checkout_path, '.'))
 
         # Try to fix the failures by removing troublesome files.
         for d in dir_info:
@@ -832,14 +836,14 @@ class SVNWrapper(SCMWrapper):
 
     # Retrieve the current HEAD version because svn is slow at null updates.
     if options.manually_grab_svn_rev and not revision:
-      from_info_live = scm.SVN.CaptureInfo(from_info['URL'])
+      from_info_live = scm.SVN.CaptureRemoteInfo(from_info['URL'])
       revision = str(from_info_live['Revision'])
       rev_str = ' at %s' % revision
 
     if from_info['URL'] != base_url:
       # The repository url changed, need to switch.
       try:
-        to_info = scm.SVN.CaptureInfo(url)
+        to_info = scm.SVN.CaptureRemoteInfo(url)
       except (gclient_utils.Error, subprocess2.CalledProcessError):
         # The url is invalid or the server is not accessible, it's safer to bail
         # out right now.
@@ -868,7 +872,7 @@ class SVNWrapper(SCMWrapper):
       else:
         if not options.force and not options.reset:
           # Look for local modifications but ignore unversioned files.
-          for status in scm.SVN.CaptureStatus(self.checkout_path):
+          for status in scm.SVN.CaptureStatus(None, self.checkout_path):
             if status[0] != '?':
               raise gclient_utils.Error(
                   ('Can\'t switch the checkout to %s; UUID don\'t match and '

presubmit_support.py
@@ -302,9 +302,8 @@ class InputApi(object):
 
       Remember to check for the None case and show an appropriate error!
     """
-    local_path = scm.SVN.CaptureInfo(depot_path).get('Path')
-    if local_path:
-      return local_path
+    return scm.SVN.CaptureLocalInfo([depot_path], self.change.RepositoryRoot()
+        ).get('Path')
 
   def LocalToDepotPath(self, local_path):
     """Translate a local path to a depot path.
@@ -315,9 +314,8 @@ class InputApi(object):
     Returns:
       The depot path (SVN URL) of the file if mapped, otherwise None.
     """
-    depot_path = scm.SVN.CaptureInfo(local_path).get('URL')
-    if depot_path:
-      return depot_path
+    return scm.SVN.CaptureLocalInfo([local_path], self.change.RepositoryRoot()
+        ).get('URL')
 
   def AffectedFiles(self, include_dirs=False, include_deletes=True,
                     file_filter=None):
@@ -429,7 +427,7 @@ class AffectedFile(object):
   """Representation of a file in a change."""
   # Method could be a function
   # pylint: disable=R0201
-  def __init__(self, path, action, repository_root=''):
+  def __init__(self, path, action, repository_root):
     self._path = path
     self._action = action
     self._local_root = repository_root
@@ -563,8 +561,8 @@ class SvnAffectedFile(AffectedFile):
 
   def ServerPath(self):
     if self._server_path is None:
-      self._server_path = scm.SVN.CaptureInfo(
-          self.AbsoluteLocalPath()).get('URL', '')
+      self._server_path = scm.SVN.CaptureLocalInfo(
+          [self.LocalPath()], self._local_root).get('URL', '')
     return self._server_path
 
   def IsDirectory(self):
@@ -575,14 +573,15 @@ class SvnAffectedFile(AffectedFile):
         # querying subversion, especially on Windows.
         self._is_directory = os.path.isdir(path)
       else:
-        self._is_directory = scm.SVN.CaptureInfo(
-            path).get('Node Kind') in ('dir', 'directory')
+        self._is_directory = scm.SVN.CaptureLocalInfo(
+            [self.LocalPath()], self._local_root
+            ).get('Node Kind') in ('dir', 'directory')
     return self._is_directory
 
   def Property(self, property_name):
     if not property_name in self._properties:
       self._properties[property_name] = scm.SVN.GetFileProperty(
-          self.AbsoluteLocalPath(), property_name).rstrip()
+          self.LocalPath(), property_name, self._local_root).rstrip()
     return self._properties[property_name]
 
   def IsTextFile(self):
@@ -593,13 +592,14 @@ class SvnAffectedFile(AffectedFile):
       elif self.IsDirectory():
         self._is_text_file = False
       else:
-        mime_type = scm.SVN.GetFileProperty(self.AbsoluteLocalPath(),
-                                            'svn:mime-type')
+        mime_type = scm.SVN.GetFileProperty(
+            self.LocalPath(), 'svn:mime-type', self._local_root)
         self._is_text_file = (not mime_type or mime_type.startswith('text/'))
     return self._is_text_file
 
   def GenerateScmDiff(self):
-    return scm.SVN.GenerateDiff([self.AbsoluteLocalPath()])
+    return scm.SVN.GenerateDiff(
+        [self.LocalPath()], self._local_root, False, None)
 
 
 class GitAffectedFile(AffectedFile):

testing_support/fake_repos.py
@@ -63,7 +63,7 @@ def commit_svn(repo, usr, pwd):
   """Commits the changes and returns the new revision number."""
   to_add = []
   to_remove = []
-  for status, filepath in scm.SVN.CaptureStatus(repo):
+  for status, filepath in scm.SVN.CaptureStatus(None, repo):
     if status[0] == '?':
       to_add.append(filepath)
     elif status[0] == '!':

tests/gcl_unittest.py
@@ -24,7 +24,7 @@ class GclTestsBase(SuperMoxTestBase):
     SuperMoxTestBase.setUp(self)
     self.fake_root_dir = self.RootDir()
     self.mox.StubOutWithMock(gcl, 'RunShell')
-    self.mox.StubOutWithMock(gcl.SVN, 'CaptureInfo')
+    self.mox.StubOutWithMock(gcl.SVN, '_CaptureInfo')
     self.mox.StubOutWithMock(gcl.SVN, 'GetCheckoutRoot')
     self.mox.StubOutWithMock(gcl, 'tempfile')
     self.mox.StubOutWithMock(gcl.upload, 'RealMain')

commit 53c12b9c227231eb31a24e7ae0a18b6de85b20e8

Fix a regression introduced in r114262, cwd must be a named argument to subprocess2.Popen().
Not sure why pylint didn't catch it. :/

TBR=dpranke@chromium.org
BUG=
TEST=


Review URL: http://codereview.chromium.org/8937010

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@114270 0039d316-1c4b-4281-b951-d872f2087c98

scm.py
@@ -226,7 +226,7 @@ class GIT(object):
     # pipe at a time.
     # The -100 is an arbitrary limit so we don't search forever.
     cmd = ['git', 'log', '-100', '--pretty=medium']
-    proc = subprocess2.Popen(cmd, cwd, stdout=subprocess2.PIPE)
+    proc = subprocess2.Popen(cmd, cwd=cwd, stdout=subprocess2.PIPE)
     url = None
     for line in proc.stdout:
       match = git_svn_re.match(line)